51ce9322ddec0a6046a5ef2eaa7a4c48d5100c72
Amil Masic <amil.masic@gmail.com>
2022-04-21 15:42:58 -0400

    added axios to the project

--- schema/schema.js
+++ schema/schema.js
@@ -1,5 +1,5 @@
 const graphql = require("graphql");
-const _ = require("lodash");
+const axios = require("axios");
 
 const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
 
@@ -12,12 +12,6 @@ const UserType = new GraphQLObjectType({
   },
 });
 
-// since we don't have a DB we are using just mock data here
-const users = [
-  { id: "23", firstName: "Bill", age: 20 },
-  { id: "43", firstName: "Samantha", age: 21 },
-];
-
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
@@ -25,7 +19,9 @@ const RootQuery = new GraphQLObjectType({
       type: UserType,
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
-        return _.find(users, { id: args.id });
+        return axios
+          .get(`http://localhost:3000/users/${args.id}`)
+          .then((response) => response.data);
       },
     },
   },
